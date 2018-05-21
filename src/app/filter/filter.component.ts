829f74b8e188b99a2cf1c1417b9498e7ca5fa42c
Thien Nguyen <thtnguyen93@gmail.com>
2018-05-21 01:21:26 -0700

    need to work on the binding for radiobutton

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilterComponent implements OnInit {
 
+  coinRank = new CoinRank();
   constructor() { }
 
   ngOnInit() {
   }
 
+  onProfitSelectionChange(entry): void {
+    if (this.coinRank.top10) { console.log('1: ' + this.coinRank.top10); }
+    if (this.coinRank.top25) { console.log('2: ' + this.coinRank.top25); }
+    if (this.coinRank.top100) { console.log('3: ' + this.coinRank.top100); }
+  }
+
+
+}
+
+export class CoinRank {
+  top10: boolean;
+  top25: boolean;
+  top100: boolean;
 }
