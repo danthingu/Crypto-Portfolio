4fd9cc79c86cb0f73fe2982148431aa8a90ea4a8
Thien (Daniel) Nguyen <danielnguyen@microvu.com>
2018-06-01 11:15:25 -0700

    add routes for register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,14 @@ import {OrderListModule} from 'primeng/primeng';
 import {SidebarModule} from 'primeng/sidebar';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CryptocurrenciesComponent } from './cryptocurrencies/cryptocurrencies.component';
+import { RegisterComponent } from './register/register.component';
 
 import { GrabCryptoServiceService } from './service/grabCryptoService.service';
 import { FilterComponent } from './filter/filter.component';
@@ -25,6 +27,10 @@ import { CarouselComponent } from './carousel/carousel.component';
 import { TransferServiceService } from './service/transferService.service';
 import { SideNavbarComponent } from './side-navbar/side-navbar.component';
 
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   declarations: [
@@ -34,7 +40,8 @@ import { SideNavbarComponent } from './side-navbar/side-navbar.component';
     CryptocurrenciesComponent,
     FilterComponent,
     CarouselComponent,
-    SideNavbarComponent
+    SideNavbarComponent,
+    RegisterComponent
 ],
   imports: [
     BrowserModule,
@@ -48,7 +55,8 @@ import { SideNavbarComponent } from './side-navbar/side-navbar.component';
     OrderListModule,
     SidebarModule,
     AngularFontAwesomeModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [GrabCryptoServiceService, TransferServiceService],
   bootstrap: [AppComponent]
